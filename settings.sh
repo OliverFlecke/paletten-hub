6bc8544315ec05b18f1a0a561bd0adb1d42715af
Oliver Fleckenstein <oliverfl@live.dk>
2021-09-02 16:23:55 +0200

    Comments on how to change MQTT broker

--- settings.sh
+++ settings.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env sh
 
 #server='palletten.northeurope.azurecontainer.io'
-server='palletten.oliverflecke.me'
+server='paletten.oliverflecke.me'
 
 port=1883
 
-# IPs for the Shelly instances should be 68, 202 and ?
-for IP in 68 202 202
+# IPs for the Shelly instances should be 68, 203 and 217. Note that these are not static. 
+# MQTT server can also be changed from the web UI which can be found by visiting each of the shellies IPs.
+
+for IP in 68 202
 do
   echo "Settings for $IP"
   curl --location --request POST "192.168.1.$IP/settings?mqtt_server=$server:$port&mqtt_enable=true&mqtt_retain=true&update_period=0"
